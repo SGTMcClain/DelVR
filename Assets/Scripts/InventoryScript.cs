a6465428310a911ca9e536d194c503beb1bbdcb5
jdawg529 <jjam529@aol.com>
2016-03-28 20:41:51 -0400

    Scripting Update

--- Assets/Scripts/InventoryScript.cs
+++ Assets/Scripts/InventoryScript.cs
@@ -1,15 +1,101 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//This is the script that holds the information of the players inventory.
+
 public class InventoryScript : MonoBehaviour {
 
+	//Boolean variables for if the player has the object or not.
+	public bool hasFireExtinguisher;
+	public bool hasFirstAidKit;
+	public bool hasCrutches;
+	public bool hasRadio;
+	public bool hasClock;
+	public bool hasBatteries;
+	public bool hasVitaminBottle;
+	public bool hasFlashlight;
+	public bool hasMedicalAlertTag;
+	public bool hasSunscreen;
+
 	// Use this for initialization
-	void Start () {
-	
+	//void Start () {
+	//
+	//}
+
+	//From the itemScript, this function can get called. It changes the boolean variable to true then displays it in the console.
+	public void getFireExtinguisher()
+	{
+		hasFireExtinguisher = true;
+		Debug.Log ("You now have the Fire Extinguisher");
 	}
-	
+
+	public void getFirstAidKit()
+	{
+		hasFirstAidKit = true;
+		Debug.Log ("You now have the First Aid Kit");
+	}
+
+	public void getRadio()
+	{
+		hasRadio = true;
+		Debug.Log ("You now have the Radio");
+	}
+
+	public void getClock()
+	{
+		hasClock = true;
+		Debug.Log ("You now have the Clock");
+	}
+
+	public void getBatteries()
+	{
+		hasBatteries = true;
+		Debug.Log ("You now have the Batteries");
+	}
+
+	public void getVitaminBottle()
+	{
+		hasVitaminBottle = true;
+		Debug.Log ("You now have the Vitamin Bottle");
+	}
+
+	public void getFlashlight()
+	{
+		hasFlashlight = true;
+		Debug.Log ("You now have the Flashlight");
+	}
+
+	public void getMedicalAlertTag()
+	{
+		hasMedicalAlertTag = true;
+		Debug.Log ("You now have the Medical Alert Tag");
+	}
+
+	public void getSunscreen()
+	{
+		hasSunscreen = true;
+		Debug.Log ("You now have the Sunscreen");
+	}
+
+	//This is an alternate way to display the information that might possible result in less lines of code needing to be written.
+	//Alternately, in the item script, it is making hasCrutches true in this script, then calling the displayInformation() function.
+	public void displayInformation()
+	{
+		if (hasCrutches) 
+		{
+			Debug.Log ("You now have the Crutches");
+		}
+	}
+
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	//void Update () {
+		//if (hasFireExtinguisher) {
+		//	Debug.Log ("You now have the fire extinguisher");
+		//}
+
+		//if (hasFirstAidKit) {
+		//	Debug.Log ("You now have the first aid kit");
+		//}
+
+	//}
 }
---
+++ Assets/Scripts/ItemScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemScript : MonoBehaviour {
+
+	//This is a script I made to pass along boolean variables to the inventory script attached to the gameManager. This will be placed on all the interactable gameObjects we plan for the player to find in our world.
+
+	//variable to hold the inventoryScript
+	public InventoryScript inventoryScript;
+
+	//When the scene loads, this will find the GameManager gameobject then find the InventoryScript inside it and attach it to the inventoryScript varaible.
+	void Awake()
+	{
+		inventoryScript = GameObject.Find ("GameManager").GetComponent<InventoryScript> ();
+	}
+		
+	void getFirstAidKit ()
+	{
+		inventoryScript.getFirstAidKit();
+	}
+
+	void getFireExtinguisher()
+	{
+		inventoryScript.getFireExtinguisher();
+	}
+
+	void getRadio()
+	{
+		inventoryScript.getRadio();
+	}
+
+	void getClock()
+	{
+		inventoryScript.getClock();
+	}
+
+	void getBatteries()
+	{
+		inventoryScript.getBatteries();
+	}
+
+	void getVitaminBottle()
+	{
+		inventoryScript.getVitaminBottle();
+	}
+
+	void getFlashlight()
+	{
+		inventoryScript.getFlashlight();
+	}
+
+	void getMedicalAlertTag()
+	{
+		inventoryScript.getMedicalAlertTag();
+	}
+
+	void getSunscreen()
+	{
+		inventoryScript.getSunscreen();
+	}
+
+
+
+	//Alternate way we can write this script, this involves setting the variable to true directly in this script, then calling a displayInformation function to show it in the inventory
+	void getCrutches()
+	{
+		inventoryScript.hasCrutches = true;
+		inventoryScript.displayInformation();
+	}
+
+
+	// Update is called once per frame
+	//void Update () {
+	//
+	//}
+}
